3229f8e36e39bcdf715880858532fd5dcb389df7
aoguz <oguzabdullah39@gmail.com>
2020-01-08 16:15:51 +0300

    butonlar eklendi

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,46 @@
 import 'package:flutter/material.dart';
- 
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Column(
-            children: <Widget>[
-              Container(
-                child: Text('gfdgfgfdg')
-
-              ,
-        ),
-        Container(
-          child: RaisedButton(
-child: Text("gönder"),
-onPressed: ()=>{
-debugPrint("tıklandı"),
-
-},
+        title: 'Material App',
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Material App Bar'),
           ),
-        )
-            ],
+          body: Center(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  child: Text('gfdgfgfdg'),
+                ),
+                Container(
+                  child: RaisedButton(
+                    child: Text("gönder"),
+                    onPressed: () => {
+                      debugPrint("tıklandı"),
+                    },
+                  ),
+                ),
+                FlatButton(
+                  child: Text("deneme1"),
+                  onPressed: () => {
+                    debugPrint("aoguz"),
+                  },
+                ),
+                FloatingActionButton(
+                  child: Icon(Icons.account_balance),
+                  onPressed: ()=>{
+                    debugPrint("deneme"),
+                
+                  },
+                )
+              ],
+            ),
           ),
-
-       
-      ),
-           
-    ));
+        ));
   }
 }
